d0d02c01909ce28d52152e0e5a8456355003deb5
Aditya Raj Sahoo <66211350+adityarajsahoo@users.noreply.github.com>
2023-04-11 13:33:18 +0530

    Added upgrade 11

---
+++ Frontend/src/pages/admin/data/data.js
@@ -0,0 +1,14 @@
+export const data = [
+    ["Year", "Jobs", "Pending", "Approved"],
+    ["2020", 1100, 500, 300],
+    ["2021", 1270, 470, 270],
+    ["2022", 1060, 320, 500],
+    ["2023", 1250, 400, 650],
+];
+
+export const options = {
+    chart: {
+        title: "Dummy Data Performance Example",
+        // subtitle: "Sales, Expenses, and Profit: 2014-2017",
+    },
+};
